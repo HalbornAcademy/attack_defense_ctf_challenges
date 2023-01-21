1d670072f08b0d7a380633d90831e134bdedd3a5
fr0zn <ferran.celades@hotmail.com>
2023-01-21 12:05:26 +0100

    Basic submite

---
+++ submiter.py
@@ -0,0 +1,38 @@
+import requests
+
+MASTER_IP = 'TODO'
+
+MY_IP = 'TODO'
+
+TOKEN = 'TODO'
+
+
+teams = requests.get(f'http://{MASTER_IP}/api/client/teams/').json()
+tasks = requests.get(f'http://{MASTER_IP}/api/client/tasks/').json()
+
+for team in teams:
+
+    print('Team: ', team['name'])
+
+    for task in tasks:
+        if team['ip'] == MY_IP:
+            continue
+        url = f"http://{team['ip']}/{task['name']}/flags"
+        response = requests.get(url)
+
+        print('Task: ', task['name'])
+
+        if 'No request made to the RPC' in response.text:
+            print('First run the exploits')
+            continue
+
+        flags = response.json()
+        print(flags)
+
+        headers = {
+        'X-Team-Token': TOKEN
+        }
+
+        result = requests.put(f'http://{MASTER_IP}/flags', json=flags, headers=headers)
+
+        print(result.text)
